6a8451d1e18c125bb28558f2ee709c77aada4a7c
Anthony Roybal <Roybal.usmc@gmail.com>
2022-04-07 09:29:42 -0700

    added comments

--- SimpleExampleOfAGameLoop/SimpleExampleOfAGameLoop.cpp
+++ SimpleExampleOfAGameLoop/SimpleExampleOfAGameLoop.cpp
@@ -6,17 +6,17 @@
 int main()
 {
 	//crude simple example
-	double lastTime = getSystemTime();
-	bool GameOver = false;
+	double lastTime = getSystemTime(); //store the time before the first run of loop
+	bool GameOver = false; //end loop 
 
-	while (!GameOver)
+	while (!GameOver)//end loop criteria
 	{
-		double currentTime = getSystemTime();
-		double deltaTime = currentTime - lastTime;
-		CheckInput();
-		Update(deltaTime);
-		Draw();
-		lastTime = currentTime;
+		double currentTime = getSystemTime();  //get current time
+		double deltaTime = currentTime - lastTime;  //gets amount of time that passed since last step of loop
+		CheckInput(); //look for user input
+		Update(deltaTime); //passes in deltatime to allow for modification to the game ie*what you see during the game
+		Draw();  //draws a new screen/image to the display
+		lastTime = currentTime; //updates lastTime inorder to keep time moving.
 	}
 }
 
