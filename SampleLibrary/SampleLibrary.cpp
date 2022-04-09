2d5ecb56a1ac4318ea93b8f46c0fe182d625b4be
Anthony Roybal <Roybal.usmc@gmail.com>
2022-04-09 07:53:26 -0700

    UsesAsTestLibrary

---
+++ SampleLibrary/SampleLibrary.cpp
@@ -0,0 +1,13 @@
+// SampleLibrary.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+void HelloWorld()
+{
+    std::cout << "Hello World!\n";
+}
+
+int main()
+{
+}
